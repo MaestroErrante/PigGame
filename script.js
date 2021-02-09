757abe7a00e8339c680e63da39626264ab3973ca
Rudyard <rudyard90@gmail.com>
2021-02-09 12:06:41 -0600

    Generate a random dice roll value

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generatin a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+
+  // 3. Check for rolled 1: if true, switch to next player
+});
