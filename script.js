b854048ef9b83e9ec13c869e97a413ecd78419a0
Rudyard <rudyard90@gmail.com>
2021-02-10 15:32:34 -0600

    add score array and define player

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
-
+let activePlayer = 0;
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generatin a random dice roll
@@ -29,8 +30,12 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
     current0El.textContent = currentScore; //Change later
   } else {
     //switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
