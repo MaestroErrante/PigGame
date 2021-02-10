2927859b4ac922230832395b796b7b2f8fadfcc0
Rudyard <rudyard90@gmail.com>
2021-02-10 15:20:37 -0600

    Add dice to random number a set score to 0

--- script.js
+++ script.js
@@ -13,12 +13,21 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generatin a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
   // 2. Display dice
   diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
 
   // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+  } else {
+    //switch to next player
+  }
 });
