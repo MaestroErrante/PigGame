fc5fb2455c6cbeb50059961e6d27b9da593d77a6
Rudyard <rudyard90@gmail.com>
2021-02-18 18:47:26 -0600

    Add function to open winners windows when player reachs 100 points

--- script.js
+++ script.js
@@ -79,6 +79,7 @@ btnHold.addEventListener('click', function () {
     // 2. Check if player's score is >= 100
     if (scores[activePlayer] >= 100) {
       // finish Game
+      openWindow();
       playing = false;
       diceEl.classList.add('hidden');
       document
